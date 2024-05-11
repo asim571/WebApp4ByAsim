336e0dc3016e95a7d8cd153f9274f34a424723ec
asim571 <160458924+asim571@users.noreply.github.com>
2024-05-11 20:27:05 +0545

    first commit

---
+++ Data/WebApp4ByAsimContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp4ByAsim.Models;
+
+namespace WebApp4ByAsim.Data
+{
+    public class WebApp4ByAsimContext : DbContext
+    {
+        public WebApp4ByAsimContext (DbContextOptions<WebApp4ByAsimContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp4ByAsim.Models.Student> Student { get; set; } = default!;
+    }
+}
